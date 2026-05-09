e7ca54751e8f1138e771b6c9d9dcb82075028f6b
mariinkys <kysgeneral.nrgua@slmail.me>
2026-05-09 20:24:45 +0200

    chore: sourceCompatibility 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
